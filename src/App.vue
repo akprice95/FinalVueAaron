256228b557da863d9170e375bfb7e0eaf44e7db4
Aaron Price <akprice95@msn.com>
2018-10-18 19:46:27 -0600

    Still not able to get it working but worked on it for about 40 minutes to try and fix

--- src/App.vue
+++ src/App.vue
@@ -16,21 +16,6 @@
           <v-card-text class="px-0">4</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 6" :key="`2${i}`" xs2>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">2</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">1</v-card-text>
-        </v-card>
-      </v-flex>
     </v-layout>
   </v-container>
 </template>
