aa584643230d6f6e879a6cb3b94a2a2c2cc27540
Aaron Price <akprice95@msn.com>
2018-10-22 15:19:07 -0600

    Struggling to figure this all out

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,6 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
-</script>
 
 <style>
 #app {
---
+++ src/components/Views/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+<v-app>
+  <v-navigation-drawer app>Nav</v-navigation-drawer>
+  <v-toolbar app>Toolbar</v-toolbar>
+  <v-content>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+  <v-footer app>Footer</v-footer>
+</v-app>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<stylescoped>
+
+</style>
