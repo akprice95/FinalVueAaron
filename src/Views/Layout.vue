47b36c5533e9aa2382f7b6360ef61acb8381f764
Aaron Price <akprice95@msn.com>
2018-10-24 15:03:20 -0600

    inclass work

--- src/Views/Layout.vue
+++ src/Views/Layout.vue
@@ -6,6 +6,7 @@
   </v-toolbar>
   <v-content>
     <v-container fluid>
+      <div>Content goes here</div>
       <router-view>Router-View</router-view>
     </v-container>
   </v-content>
--- src/components/router.js
+++ src/components/router.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import Layout from "./views/Layout.vue"
+import Layout from "./views/Layout.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
