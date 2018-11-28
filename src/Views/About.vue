276f665f9e7eebd4c853ed1a8fdc0a7c3ccefeba
Thor Anderson <ta.anderson@gmail.com>
2018-11-28 10:03:25 -0700

    Fixed an issue with double #app reference.  Need epic-nickname

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -1,59 +1,64 @@
 <template>
-    <Div>
-        <div>
-            <div>
-                <h1>
-                    <div>
-                        <label>Username</label>
-                        <input class="form-control" type="text" v-model="user.username">
-                    </div>
-                    <div>
-                        <label>Mail</label>
-                        <input class="form-control" type="text" v-model="user.email">
-                        <button class="btn btn-primary" @click="submit">Submit</button>
-                          <button @click="fetchData">Get Data</button>
-                          <ul>
-                            <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email}}</li>
-
-                          </ul>
-                    </div>
-                </h1>
-            </div>
-        </div>
-    </Div>
+  <Div>
+    <div>
+      <div>
+        <h1>
+          <div>
+            <label>Username</label>
+            <input class="form-control" type="text" v-model="user.username">
+          </div>
+          <div>
+            <label>Mail</label>
+            <input class="form-control" type="text" v-model="user.email">
+            <button class="btn btn-primary" @click="submit">Submit</button>
+            <button @click="fetchData">Get Data</button>
+            <ul>
+              <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email}}</li>
+            </ul>
+          </div>
+        </h1>
+      </div>
+    </div>
+  </Div>
 </template>
 
 <script>
-import { films } from '../assets/films'
-import { starships } from '../assets/starships'
+import { films } from "../assets/films";
+import { starships } from "../assets/starships";
 export default {
   data() {
     return {
       user: {
-        username: '',
-        email: ''
+        username: "",
+        email: ""
       },
       users: []
     };
   },
-  methods:{
+  methods: {
     submit() {
-      this.$http.post('https://vuejs-test-f75d2.firebaseio.com/data.json', this.user)
-.then(response => {
-  console.log(response)
-  }, error => {
-    console.log(error);
-  });
-},
-fetchData() {
-  this.$http.get('https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}')
-  .then(response => {
-        return response.json();
-       })
-       .then(data => console.log(data));
-}
+      this.$http
+        .post("https://vuejs-test-f75d2.firebaseio.com/data.json", this.user)
+        .then(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    },
+    fetchData() {
+      fetch(
+          "https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}"
+        )
+        .then(response => {
+          return response.json();
+        })
+        .then(data => console.log(data));
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -46,16 +46,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
-new Vue({
-  el: '#app',
-  data: () => ({
-    drawer: true
-  }),
-
-  props: {
-    source: String
-  }
-})
-
-Vue.use(Vuetify)
