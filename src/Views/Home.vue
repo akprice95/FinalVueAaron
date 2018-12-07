e718cf58c366e5dd96f807d0152d1d2c228b6114
Bronson Allen <bronson.allen@mx.com>
2018-12-06 18:05:13 -0700

    UI updates Aaron Price

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container id="dropdown-example" grid-list-xl>
     <v-layout row wrap>
-      
+
       <v-toolbar>
         <v-flex xs12 sm3>
           <v-select v-model="Platform" :items="items" label="Platform"></v-select>
@@ -10,20 +10,20 @@
         <button @click="fetchData">Get Data</button>
       </v-toolbar>
         <v-flex v-for="(Player,index)  in playerStats" :key="index" xs3>
-          <v-card>
-       <v-card-text>   
-    <p class="heading"> {{Player.key}}</p>
-    <p class="score">{{Player.value}}</p>
-    <p></p>
-       </v-card-text>
+          <v-card class="v-card-animation">
+           <v-card-text>
+            <p class="heading"> {{Player.key}}</p>
+            <p class="score">{{Player.value}}</p>
+            <p></p>
+           </v-card-text>
           </v-card>
         </v-flex>
-    
-        
+
+
     </v-layout>
   </v-container>
 
-  
+
 </template>
 
 
@@ -55,9 +55,9 @@ export default {
     }),
 methods:{
     fetchData() {
-      let APICall = ('https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/' + this.Platform + '/' + this.Username) 
+      let APICall = ('https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/' + this.Platform + '/' + this.Username)
       let request = new Request(APICall,{
-	method: 'GET', 
+	method: 'GET',
 	headers: new Headers({
 		'TRN-Api-Key': '7152cd0a-ff37-4945-b2ac-ea8d1c4f3fe2'
 	})
@@ -79,7 +79,31 @@ methods:{
 
 <style>
 .heading{
-  font: 24px;
-  color: green;
+  text-align: center;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 13px;
+  color: #09eae2;
+  text-shadow: 2px 3px 6px #000000;
+}
+
+.score{
+  text-align: center;
+    font-size: 28px;
+    font-weight: bold;
+    text-shadow: 2px 8px 10px #000000;
+}
+
+.v-card-animation{
+  -webkit-transition: width 2s, height 2s; /* For Safari 3.1 to 6.0 */
+  transition: width 2s, height 2s;
+  box-shadow: 10px 10px 12px #000000;
+}
+
+.v-card-animation:hover{
+  background-color: #09eae229 !important;
+  width: 102%;
+  height: 102%;
+
 }
 </style>
